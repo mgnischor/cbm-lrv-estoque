f63194298b1b4325e03af9f7afe3c6661e208459
Miguel Nischor <miguel@nischor.com.br>
2026-05-13 12:05:27 -0400

    code: adiciona arquivo App.xaml.cs como ponto de entrada da aplicação WPF

---
+++ Source/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace CEB;
+
+/// <summary>
+/// Ponto de entrada da aplicação WPF de controle de estoque com endereçamento.
+/// </summary>
+public partial class App : Application { }
